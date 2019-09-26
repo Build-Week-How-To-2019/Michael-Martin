7c20acf28d242b3a5c61b30b794053f247c58df5
littleonetwo <littleonetwo@gmail.com>
2019-09-26 12:05:08 -0500

    final version with dark mode working!!

--- main.js
+++ main.js
@@ -2,25 +2,48 @@
 
 /* add the logic here for a switch that will toggle the theme of the site*/
 
-const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
-const currentTheme = localStorage.getItem('theme');
+document.addEventListener('DOMContentLoaded', function(){
 
-if (currentTheme) {
-    document.documentElement.setAttribute('data-theme', currentTheme);
+  let bgCa= getComputedStyle(document.documentElement).getPropertyValue('--background-color-1');
+  let bgC2a= getComputedStyle(document.documentElement).getPropertyValue('--background-color-2');
+  let fontCa= getComputedStyle(document.documentElement).getPropertyValue('--font-color-active');
+  let theme = 'unknown';
 
-    if (currentTheme === 'dark') {
-        toggleSwitch.checked = true;
+  if(fontCa === ' black'){
+    theme = 'light';
+    //console.log('here!');
+  } else if (fontCa === ' #696e6d'){
+    theme = 'dark';
+  };
+
+  //console.log(fontCa);
+  //console.log(theme);
+
+  var toggleSwitch = document.querySelector('input[type="checkbox"]');
+
+
+  toggleSwitch.addEventListener('change', function(){
+    if(toggleSwitch.checked && theme === 'light') {
+      //console.log('switch is on');
+      document.documentElement.style.setProperty('--background-color-1',' #000000');
+      document.documentElement.style.setProperty('--background-color-2',' #1c1c1c');
+      document.documentElement.style.setProperty('--font-color-active',' #696e6d');
+      document.documentElement.style.setProperty('--searchbar-bg', ' white');
+      document.documentElement.style.setProperty('--searchbar-f', 'black');
+      theme = 'dark';
+
+    } else {
+      //console.log('switch is off');
+      document.documentElement.style.setProperty('--background-color-1',' white');
+      document.documentElement.style.setProperty('--background-color-2',' rgb(61, 126, 153)');
+      document.documentElement.style.setProperty('--font-color-active',' black');
+      document.documentElement.style.setProperty('--searchbar-bg', ' #000000');
+      document.documentElement.style.setProperty('--searchbar-f', ' #696e6d');
+      theme = 'light';
     }
-}
+  });
 
-function switchTheme(e) {
-    if (e.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        localStorage.setItem('theme', 'dark');
-    }
-    else {        document.documentElement.setAttribute('data-theme', 'light');
-          localStorage.setItem('theme', 'light');
-    }
-}
 
-toggleSwitch.addEventListener('change', switchTheme, false);
+
+
+});
